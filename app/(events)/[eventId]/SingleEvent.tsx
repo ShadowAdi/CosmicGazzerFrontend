5c0272f181224702475f44474eb785647d2e9d79
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-20 20:28:30 +0530

    added screen for single event

---
+++ app/(events)/[eventId]/SingleEvent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SingleEvent = () => {
+  return (
+    <View>
+      <Text>SingleEvent</Text>
+    </View>
+  )
+}
+
+export default SingleEvent
+
+const styles = StyleSheet.create({})
